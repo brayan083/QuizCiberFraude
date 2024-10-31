99cd7805f11ae62142f1aca473addd6b6fe2c62c
Brayan Z. <zorrobrayan0@gmail.com>
2024-10-30 22:03:29 -0300

    Agregar comentario para identificar un cambio en el formulario del cuestionario

--- components/quiz-form.tsx
+++ components/quiz-form.tsx
@@ -33,6 +33,7 @@ export function QuizForm({ score, totalQuestions, userName }: QuizFormProps) {
       //   totalQuestions,
       //   ...formData,
       // });
+      // soy el cambio
 
       // Send email using Resend API
       const emailBody = {
