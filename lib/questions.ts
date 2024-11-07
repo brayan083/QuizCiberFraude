79db7eaaf1f5d1237f5a1c461930c0ffb7475df8
Brayan Z. <zorrobrayan0@gmail.com>
2024-11-07 12:57:15 -0300

    Agregar preguntas sobre fraudes y estafas en la sección de preguntas

--- lib/questions.ts
+++ lib/questions.ts
@@ -19,40 +19,40 @@ export const questions = [
     explanationYes: "Podría ser debido a clientes estafados a través de tiendas falsas.",
     explanationNo: "Puede que no hayas detectado este problema aún, pero es importante mantenerse alerta. La suplantación de marca es una táctica común de los ciberdelincuentes y puede surgir en cualquier momento, afectando la confianza de tus clientes, los ingresos y la reputación de la marca."
   },
-  // {
-  //   type: "yesno",
-  //   question: "¿Has notado una bajada de ingresos y no sabes de dónde viene, bajadas de tráfico puntuales o bajadas en retornos en campañas de publicidad?",
-  //   correctAnswer: true,
-  //   explanationYes: "Si es así, puede que haya campañas de anuncios falsos dirigiendo a tus usarios a tiendas clonadas para ser estafados.",
-  //   explanationNo: "Es genial que no hayas notado cambios de este tipo, aunque te animamos a monitorizar tus informes de tráfico e ingresos teniendo en cuenta este problema real. Cambios inesperados podrían ser la primera señal de actividades fraudulentas."
-  // },
-  // {
-  //   type: "yesno",
-  //   question: "¿Te han pedido los usuarios un descuento u oferta que tú no has anunciado?",
-  //   correctAnswer: true,
-  //   explanationYes: "Es probable que los usuarios hayan visto esta oferta tan llamativa a través de anuncios en redes sociales. ",
-  //   explanationNo: "Es importante estar atento a cualquier mención de ofertas o descuentos que tu empresa no haya autorizado en un futuro. Estos pueden ser signos de campañas fraudulentas que usan tu marca para engañar a los consumidores."
-  // },
-  // {
-  //   type: "yesno",
-  //   question: "¿El personal de tus tiendas físicas es preguntado por pedidos que no llegan?",
-  //   correctAnswer: true,
-  //   explanationYes: "En esta ocasión podría deberse a clientes que creen haber comprado en la marca oficial pero que en realidad han sido estafados. ",
-  //   explanationNo: "Aunque no se haya presentado esta situación, es importante capacitar al personal de las tiendas físicas para identificar y manejar adecuadamente este tipo de consultas, ya que podrían ser indicadores de actividades fraudulentas en línea que afectan a la marca."
-  // },
-  // {
-  //   type: "yesno",
-  //   question: "¿Has visto anuncios que parecen de tu marca pero no estas seguro de si son tuyos?",
-  //   correctAnswer: true,
-  //   explanationYes: "Podrían ser anuncios falsos que solo se pueden ver bajo ciertas condiciones. Si estas se cumplen, los usuarios pueden acabar en una tienda falsa creyendo que es la tienda oficial.",
-  //   explanationNo: "Es crucial mantener una vigilancia constante aunque no se hayan detectado anuncios falsos. Los ciberdelincuentes a menudo utilizan técnicas sofisticadas para mostrar anuncios falsos solo a ciertos usuarios o en determinadas circunstancias, lo que dificulta su detección."
-  // },
-  // {
-  //   type: "yesno",
-  //   question: "¿Has visto algún dominio que usa tu marca como por ejemplo tumarca.shop, tumarca-outlet.es pero al entrar en el dominio este no funciona o da algún error?",
-  //   correctAnswer: true,
-  //   explanationYes: "Los dominios que usan los ciberdelincuentes para suplatar una marca pueden no funcionar cuando los encuentras o dar un error. Esto no implica que bajo otras condiciones o para otros usuarios sí sean accesibles.",
-  //   explanationNo: "Aunque no hayas encontrado dominios sospechosos, es importante realizar búsquedas regulares de dominios que puedan estar usando tu marca. Los ciberdelincuentes a menudo registran dominios similares a los de marcas conocidas para usarlos en sus campañas de fraude, incluso si no están activos todo el tiempo."
-  // },
+  {
+    type: "yesno",
+    question: "¿Has notado una bajada de ingresos y no sabes de dónde viene, bajadas de tráfico puntuales o bajadas en retornos en campañas de publicidad?",
+    correctAnswer: true,
+    explanationYes: "Si es así, puede que haya campañas de anuncios falsos dirigiendo a tus usarios a tiendas clonadas para ser estafados.",
+    explanationNo: "Es genial que no hayas notado cambios de este tipo, aunque te animamos a monitorizar tus informes de tráfico e ingresos teniendo en cuenta este problema real. Cambios inesperados podrían ser la primera señal de actividades fraudulentas."
+  },
+  {
+    type: "yesno",
+    question: "¿Te han pedido los usuarios un descuento u oferta que tú no has anunciado?",
+    correctAnswer: true,
+    explanationYes: "Es probable que los usuarios hayan visto esta oferta tan llamativa a través de anuncios en redes sociales. ",
+    explanationNo: "Es importante estar atento a cualquier mención de ofertas o descuentos que tu empresa no haya autorizado en un futuro. Estos pueden ser signos de campañas fraudulentas que usan tu marca para engañar a los consumidores."
+  },
+  {
+    type: "yesno",
+    question: "¿El personal de tus tiendas físicas es preguntado por pedidos que no llegan?",
+    correctAnswer: true,
+    explanationYes: "En esta ocasión podría deberse a clientes que creen haber comprado en la marca oficial pero que en realidad han sido estafados. ",
+    explanationNo: "Aunque no se haya presentado esta situación, es importante capacitar al personal de las tiendas físicas para identificar y manejar adecuadamente este tipo de consultas, ya que podrían ser indicadores de actividades fraudulentas en línea que afectan a la marca."
+  },
+  {
+    type: "yesno",
+    question: "¿Has visto anuncios que parecen de tu marca pero no estas seguro de si son tuyos?",
+    correctAnswer: true,
+    explanationYes: "Podrían ser anuncios falsos que solo se pueden ver bajo ciertas condiciones. Si estas se cumplen, los usuarios pueden acabar en una tienda falsa creyendo que es la tienda oficial.",
+    explanationNo: "Es crucial mantener una vigilancia constante aunque no se hayan detectado anuncios falsos. Los ciberdelincuentes a menudo utilizan técnicas sofisticadas para mostrar anuncios falsos solo a ciertos usuarios o en determinadas circunstancias, lo que dificulta su detección."
+  },
+  {
+    type: "yesno",
+    question: "¿Has visto algún dominio que usa tu marca como por ejemplo tumarca.shop, tumarca-outlet.es pero al entrar en el dominio este no funciona o da algún error?",
+    correctAnswer: true,
+    explanationYes: "Los dominios que usan los ciberdelincuentes para suplatar una marca pueden no funcionar cuando los encuentras o dar un error. Esto no implica que bajo otras condiciones o para otros usuarios sí sean accesibles.",
+    explanationNo: "Aunque no hayas encontrado dominios sospechosos, es importante realizar búsquedas regulares de dominios que puedan estar usando tu marca. Los ciberdelincuentes a menudo registran dominios similares a los de marcas conocidas para usarlos en sus campañas de fraude, incluso si no están activos todo el tiempo."
+  },
 
 ];
